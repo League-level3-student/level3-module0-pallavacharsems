7dc76936707c65c87a326d50b72a337e28fb4777
Pallav <Pallav@DESKTOP-DVPQU53>
2021-10-21 20:01:41 -0700

    Almost finished!

--- src/_03_Intro_to_2D_Arrays/_02_2DArrayPractice.java
+++ src/_03_Intro_to_2D_Arrays/_02_2DArrayPractice.java
@@ -24,168 +24,193 @@ package _03_Intro_to_2D_Arrays;
  *  Column 0 = matrix[0][0], matrix[1][0], matrix[2][0] // 1, 4, 7
  */
 public class _02_2DArrayPractice {
-    /*
-     * Return a 2D array that can accommodate the grid of values below:
-     *          0, 1, 2
-     *          3, 4, 5
-     *          6, 7, 8
-     */
-    public Integer[][] test1() {
-        Integer [][] array2 = new Integer [3][3];
-        return array2;
-    }
-    
-    /*
-     * Return a 2D array that can accommodate the grid of values below:
-     *          "a", "b", "c", "d",
-     *          "e", "f", "g", "e"
-     */
-    public String[][] test2() {
-        String [][] string = new String[2][4];
-        return string;
-    }
-    
-    /*
-     * Return a 2D array that can accommodate the grid of values below:
-     *          true, false,
-     *          false, false,
-     *          false, true,
-     *          true, true,
-     *          true, false
-     */
-    public Boolean[][] test3() {
-   
-        return new Boolean[5][2];
-    }
-    
-    /*
-     * Draw the grid of values for the following 2D array of ints:
-     * Assume all the elements in the 2D array are initialized to random ints
-     * 
-     * int[][] grid = new int[2][3];
-     * 
-     * 
-     * 
-     * Show your teacher the grid you created
-     */
-    
-    /*
-     * Draw the grid of values for the following 2D array of Robots
-     * Assume all the elements in the 2D array are initialized.
-     * 
-     * Robot[][] robots = new Robot[4][2];
-     * 
-     * 
-     * 
-     * Show your teacher the grid you created
-     */
-    
-    /* Given the array of Objects below, return the element that is not null
-     *          null, null, null, null
-     *          null, null, null, Object
-     */
-    public Object test4() {
-        Object[][] objects = { {null, null, null, null}, {null, null, null, new Object()} };
-        
-        return objects[1][3];
-    }
-    
-    /* Given the array of Objects below, return the element that is not null
-     *          null, null, Object, null
-     *          null, null, null, null
-     */
-    public Object test5() {
-        Object[][] objects = { {null, null, new Object(), null}, {null, null, null, null} };
-        
-        return objects[0][2];
-    }
-    
-    /*
-     * Write an algorithm to return the sum of all the numbers in the
-     * first row (row 0)
-     */
-    public Integer test6() {
-        int[][] nums = { {8, 7, 6, 5}, {4, 3}, {2, 1, 0} };
-        Integer sum = 0;
-        for (int i = 0; i < nums[0].length; i++) {
-        	
-        	sum+=nums[0][i];
+	/*
+	 * Return a 2D array that can accommodate the grid of values below: 0, 1, 2 3,
+	 * 4, 5 6, 7, 8
+	 */
+	public Integer[][] test1() {
+		Integer[][] array2 = new Integer[3][3];
+		return array2;
+	}
+
+	/*
+	 * Return a 2D array that can accommodate the grid of values below: "a", "b",
+	 * "c", "d", "e", "f", "g", "e"
+	 */
+	public String[][] test2() {
+		String[][] string = new String[2][4];
+		return string;
+	}
+
+	/*
+	 * Return a 2D array that can accommodate the grid of values below: true, false,
+	 * false, false, false, true, true, true, true, false
+	 */
+	public Boolean[][] test3() {
+
+		return new Boolean[5][2];
+	}
+
+	/*
+	 * Draw the grid of values for the following 2D array of ints: Assume all the
+	 * elements in the 2D array are initialized to random ints
+	 * 
+	 * int[][] grid = new int[2][3];
+	 * 
+	 * 
+	 * 
+	 * Show your teacher the grid you created
+	 */
+
+	/*
+	 * Draw the grid of values for the following 2D array of Robots Assume all the
+	 * elements in the 2D array are initialized.
+	 * 
+	 * Robot[][] robots = new Robot[4][2];
+	 * 
+	 * 
+	 * 
+	 * Show your teacher the grid you created
+	 */
+
+	/*
+	 * Given the array of Objects below, return the element that is not null null,
+	 * null, null, null null, null, null, Object
+	 */
+	public Object test4() {
+		Object[][] objects = { { null, null, null, null }, { null, null, null, new Object() } };
+
+		return objects[1][3];
+	}
+
+	/*
+	 * Given the array of Objects below, return the element that is not null null,
+	 * null, Object, null null, null, null, null
+	 */
+	public Object test5() {
+		Object[][] objects = { { null, null, new Object(), null }, { null, null, null, null } };
+
+		return objects[0][2];
+	}
+
+	/*
+	 * Write an algorithm to return the sum of all the numbers in the first row (row
+	 * 0)
+	 */
+	public Integer test6() {
+		int[][] nums = { { 8, 7, 6, 5 }, { 4, 3 }, { 2, 1, 0 } };
+		Integer sum = 0;
+		for (int i = 0; i < nums[0].length; i++) {
+
+			sum += nums[0][i];
 		}
-        
-        		
-        return sum;
-    }
-    
-    /*
-     * Write an algorithm to return the sum of all the numbers in the
-     * first column (column 0)
-     */
-    public Integer test7() {
-        int[][] nums = { {8, 7, 6, 5}, {4, 3}, {2, 1, 0} };
-        Integer sum = 0;
-        for (int i = 0; i < nums.length; i++) {
-        	sum+=nums[i][0];
+
+		return sum;
+	}
+
+	/*
+	 * Write an algorithm to return the sum of all the numbers in the first column
+	 * (column 0)
+	 */
+	public Integer test7() {
+		int[][] nums = { { 8, 7, 6, 5 }, { 4, 3 }, { 2, 1, 0 } };
+		Integer sum = 0;
+		for (int i = 0; i < nums.length; i++) {
+			sum += nums[i][0];
 		}
-        return sum;
-    }
-    
-    /*
-     * Write an algorithm to return the sum of all the numbers in the
-     * specified row
-     */
-    public Integer getSumByRow(int[][] matrix, int row) {
-        Integer sum = 0;
-        for (int i = 0; i < matrix[row].length; i++) {
-        	sum+=matrix[row][i];
-		}	
-        return sum;
-    }
-    
-    /*
-     * Write an algorithm to return the sum of all the numbers in the
-     * specified column
-     */
-    public Integer getSumByColumn(int[][] matrix, int col) {
-        
-        return null;
-    }
-    
-    /*
-     * Return the value of the element adjacent to the specified row and column
-     * The directions East, West, North, South are relative to the 2D array
-     * matrix/grid. For example in this 3x3 2D array:
-     * 
-     *          [1][2][3]
-     *          [4][5][6]
-     *          [7][8][9]
-     * 
-     * for the 5 in the middle (row = 1, column = 1), then
-     * East neighbor = 6
-     * West neighbor = 4
-     * North neighbor = 2
-     * South neighbor = 8
-     * 
-     * 1. Assume input row and column values are valid indexes in the matrix.
-     * 2. If the neighbor does not exist, i.e. it's outside the grid
-     *    boundaries, return null.
-     */
-    public Integer getEastNeighbor(int[][] matrix, int row, int col) {
-        
-        return null;
-    }
-    
-    public Integer getWestNeighbor(int[][] matrix, int row, int col) {
-        
-        return null;
-    }
-    
-    public Integer getNorthNeighbor(int[][] matrix, int row, int col) {
-        
-        return null;
-    }
-    
-    public Integer getSouthNeighbor(int[][] matrix, int row, int col) {
-        
-        return null;
-    }
+		return sum;
+	}
+
+	/*
+	 * Write an algorithm to return the sum of all the numbers in the specified row
+	 */
+	public Integer getSumByRow(int[][] matrix, int row) {
+		Integer sum = 0;
+		for (int i = 0; i < matrix[row].length; i++) {
+			sum += matrix[row][i];
+		}
+		return sum;
+	}
+
+	/*
+	 * Write an algorithm to return the sum of all the numbers in the specified
+	 * column
+	 */
+	public Integer getSumByColumn(int[][] matrix, int col) {
+
+		Integer sum = 0;
+		for (int i = 0; i < matrix.length; i++) {
+			sum += matrix[i][col];
+		}
+		return sum;
+	}
+
+	/*
+	 * Return the value of the element adjacent to the specified row and column The
+	 * directions East, West, North, South are relative to the 2D array matrix/grid.
+	 * For example in this 3x3 2D array:
+	 * 
+	 * [1][2][3] [4][5][6] [7][8][9]
+	 * 
+	 * for the 5 in the middle (row = 1, column = 1), then East neighbor = 6 West
+	 * neighbor = 4 North neighbor = 2 South neighbor = 8
+	 * 
+	 * 1. Assume input row and column values are valid indexes in the matrix. 2. If
+	 * the neighbor does not exist, i.e. it's outside the grid boundaries, return
+	 * null.
+	 */
+	public Integer getEastNeighbor(int[][] matrix, int row, int col) {
+		if (row < matrix.length && row >= 0) {
+			if (col+1 < matrix[row].length && col+1 >= 0) {
+				return matrix[row][col + 1];
+			} else {
+				return null;
+			}
+		} else {
+			return null;
+
+		}
+	}
+
+	public Integer getWestNeighbor(int[][] matrix, int row, int col) {
+
+		if (row < matrix.length && row >= 0) {
+			if (col-1 < matrix[row].length && col-1 >= 0) {
+				return matrix[row][col -1];
+			} else {
+				return null;
+			}
+		} else {
+			return null;
+
+		}
+	}
+
+	public Integer getNorthNeighbor(int[][] matrix, int row, int col) {
+
+		if (row-1 < matrix.length && row-1 >= 0) {
+			if (col < matrix[row].length && col >= 0) {
+				return matrix[row-1][col];
+			} else {
+				return null;
+			}
+		} else {
+			return null;
+
+		}
+	}
+
+	public Integer getSouthNeighbor(int[][] matrix, int row, int col) {
+
+		if (row+1 < matrix.length && row+1 >= 0) {
+			if (col < matrix[row].length && col >= 0) {
+				return matrix[row+1][col];
+			} else {
+				return null;
+			}
+		} else {
+			return null;
+
+		}
+	}
 }
